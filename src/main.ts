36f90fd2735a85bd352aff0d2c795b3d8d27dfd1
Marcuth <thiago.playgames15@gmail.com>
2024-09-14 12:00:04 -0300

    Adicionando Licença e habilitando cors

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,12 @@ async function bootstrap() {
         }),
     )
 
+    app.enableCors({
+        origin: true,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+        credentials: true,
+    })
+
     await app.listen(port)
 }
 
