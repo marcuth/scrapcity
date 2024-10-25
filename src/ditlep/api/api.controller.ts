dca3e254f1a5bb94a0113fb33111e45731ee15e7
Remote <remote.marcuth@gmail.com>
2024-10-25 20:17:03 -0300

    melhorando a documentação API

--- src/ditlep/api/api.controller.ts
+++ src/ditlep/api/api.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Query } from "@nestjs/common"
+import { ApiQuery } from "@nestjs/swagger";
 
 @Controller("ditlep/api")
 export class DitlepApiController {
@@ -9,6 +10,15 @@ export class DitlepApiController {
     async getDragonTV() {}
 
     @Get("dragons")
+    @ApiQuery({ name: "nameOrId", required: false })
+    @ApiQuery({ name: "rarities", required: false })
+    @ApiQuery({ name: "elements", required: false })
+    @ApiQuery({ name: "pageNumber", required: false })
+    @ApiQuery({ name: "pageSize", required: false })
+    @ApiQuery({ name: "category", required: false })
+    @ApiQuery({ name: "inStore", required: false })
+    @ApiQuery({ name: "isBreedable", required: false })
+    @ApiQuery({ name: "tag", required: false })
     async getDragons(
         @Query("nameOrId") nameOrId?: string | number,
         @Query("rarities") rarities?: string[],
@@ -22,6 +32,11 @@ export class DitlepApiController {
     ) {}
 
     @Get("items")
+    @ApiQuery({ name: "nameOrId", required: false })
+    @ApiQuery({ name: "sort", required: false })
+    @ApiQuery({ name: "pageNumber", required: false })
+    @ApiQuery({ name: "pageSize", required: false })
+    @ApiQuery({ name: "group", required: false })
     async getItems(
         @Query("nameOrId") nameOrId?: string | number,
         @Query("sort") sort?: string,
@@ -40,23 +55,30 @@ export class DitlepApiController {
     async getTemporaryQuests() {}
 
     @Get("events/fog-islands")
+    @ApiQuery({ name: "id", required: false })
     async getFogIslandsEvent(@Query("id") id?: string) {}
 
     @Get("events/grid-islands")
+    @ApiQuery({ name: "id", required: false })
     async getGridIslandsEvent(@Query("id") id?: string) {}
 
     @Get("events/heroic-races")
+    @ApiQuery({ name: "id", required: false })
     async getHeroicRacesEvent(@Query("id") id?: string) {}
 
     @Get("events/maze-islands")
+    @ApiQuery({ name: "id", required: false })
     async getMazeIslandsEvent(@Query("id") id?: string) {}
 
     @Get("events/puzzle-islands")
+    @ApiQuery({ name: "id", required: false })
     async getPuzzleIslandsEvent(@Query("id") id?: string) {}
 
     @Get("events/runner-islands")
+    @ApiQuery({ name: "id", required: false })
     async getRunnerIslandsEvent(@Query("id") id?: string) {}
 
     @Get("events/tower-islands")
+    @ApiQuery({ name: "id", required: false })
     async getTowerIslandsEvent(@Query("id") id?: string) {}
 }
