518f374f099c020724069bc91cb4258370a5524d
Remote <remote.marcuth@gmail.com>
2024-10-25 20:01:21 -0300

    ajeitando o código

---
+++ src/helpers/config.helper.ts
@@ -0,0 +1,22 @@
+import { ValidationPipe } from "@nestjs/common"
+import * as dotenv from "dotenv"
+
+dotenv.config()
+
+const configHelper = {
+    app: {
+        port: process.env.PORT || 3003,
+        validationPipe: new ValidationPipe({
+            whitelist: true,
+            transform: true,
+            skipMissingProperties: false,
+        }),
+        corsOptions: {
+            origin: true,
+            methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+            credentials: true,
+        }
+    },
+}
+
+export default configHelper
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,15 @@
-import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
-import * as dotenv from "dotenv"
 
 import { AppModule } from "./app.module"
-
-dotenv.config()
+import configHelper from "./helpers/config.helper"
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
-    const port = process.env.PORT ?? 3000
 
-    app.useGlobalPipes(
-        new ValidationPipe({
-            whitelist: true,
-            transform: true,
-            skipMissingProperties: false,
-        }),
-    )
+    app.useGlobalPipes(configHelper.app.validationPipe)
+    app.enableCors(configHelper.app.corsOptions)
 
-    app.enableCors({
-        origin: true,
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-        credentials: true,
-    })
-
-    await app.listen(port)
+    await app.listen(configHelper.app.port)
 }
 
 bootstrap()
